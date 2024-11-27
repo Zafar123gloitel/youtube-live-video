6ad5e3a90b46fede7c90c4f4ea61ecf9273f6105
Gloitel <zafaryab.khan@gloitel.in>
2024-11-27 13:52:47 +0530

    [ADD] build command

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import tw from 'twrnc';
 const App = () => {
   return (
     <View style={tw`bg-white flex-1`}>
-      <Text style={tw`text-black text-xl `}>App Screen</Text>
+      <Text style={tw`text-black text-xl text-center mt-10 font-bold `}>
+        App Screen
+      </Text>
     </View>
   );
 };
